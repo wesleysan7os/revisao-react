f363ebf6a629716e3b27b7f5440d7744a9806c94
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-10-27 23:59:25 -0300

    resolving exercises about lists

---
+++ pages/basicos/lista3.jsx
@@ -0,0 +1,12 @@
+function gerarLista(final = 10) {
+  let lista = [];
+  for (let i = 1; i <= final; i++) {
+    lista.push(<span>{i},</span>);
+  }
+
+  return lista;
+}
+
+export default function lista() {
+  return <div>{gerarLista(20)}</div>;
+}
