68a8733c7e37905f645e8588127f9274552d9836
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-10-28 21:37:54 -0300

    reusing component Titulo

---
+++ components/Titulo.jsx
@@ -0,0 +1,8 @@
+export default function Titulo({ titulo, subtitulo }) {
+  return (
+    <div>
+      <h1>{titulo}</h1>
+      <h2>{subtitulo}</h2>
+    </div>
+  );
+}
---
+++ pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo";
+
+export default function usandoTitulo() {
+  return (
+    <>
+      <Titulo titulo="ALo amor" subtitulo="teste123" />
+      <Titulo titulo="Hello boy" subtitulo="My name is Sol" />
+    </>
+  );
+}
